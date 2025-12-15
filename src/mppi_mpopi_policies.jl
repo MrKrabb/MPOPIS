eeb4d068d39262f19e298783b182b49e6e895e29
jprehbein <janphilip.rehbein@gmail.com>
2025-12-15 15:06:21 +0100

    some updates

--- src/mppi_mpopi_policies.jl
+++ src/mppi_mpopi_policies.jl
@@ -485,7 +485,7 @@ function calculate_trajectory_costs(pol::Data_Policy, env::AbstractEnv)
 
     # Statt Normalverteilung -> Replay Buffer
     center = pol.g_prev === nothing ? pol.g_mean : pol.g_prev
-    sampled_actions, sampled_outputs, g, Hmat_actions = sample_trajectories(pol.H, x0, K, T; g_mean=center, σ_g=0.5, rng=pol.rng)
+    sampled_actions, sampled_outputs, g, Hmat_actions = sample_trajectories(pol.H, x0, K, T; g_mean=center, σ_g=0.1, rng=pol.rng)
     P = Distributions.MvNormal(pol.Σ)
     E = rand(pol.rng, P, K, T)
     trajectory_cost = zeros(Float64, K)
